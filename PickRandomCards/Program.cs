13db2d8ba98ebfcae85454eaf96752a3d6500a72
Bartek <bartlomiej.kacki@gmail.com>
2024-05-16 19:13:46 +0200

    Dodaj pliki projektów.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wpisz liczbę generowanych kart: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                    Console.WriteLine(card);
+
+            }
+            else
+                Console.WriteLine("Nie można wyświetlić kart, ponieważ podana wartość nie jest liczbą.");
+        }
+    }
+}
